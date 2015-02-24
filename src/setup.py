1c803e8288a5fe33643afd1339bad4153e8c473c
Julian Garcia <julian.garcia@monash.edu>
2015-02-24 15:39:49 +1100

    Fix setup.py

--- src/setup.py
+++ src/setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 import numpy
 
 setup(
-    name = "Paramless",
-    ext_modules = cythonize('paramless_cython.pyx', include_path = [numpy.get_include()]),
+    name="Paramless",
+    ext_modules=cythonize(
+        'paramless_cython.pyx'), include_dirs=[numpy.get_include()]
 )
